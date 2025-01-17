9805491f6d33a0cd9d76f1323931f8b4d86182ba
Dumitru <dima50066@gmail.com>
2025-01-17 14:18:46 +0100

    add styles to nannies blocks

--- frontend/src/components/nanny/nannyInfo/NannyInfo.tsx
+++ frontend/src/components/nanny/nannyInfo/NannyInfo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Nanny as NannyType } from "../../../types";
+import Icon from "../../../shared/icon/Icon";
 
 interface NannyProps {
   nanny: NannyType;
@@ -7,32 +8,79 @@ interface NannyProps {
 
 const Nanny: React.FC<NannyProps> = ({ nanny }) => {
   return (
-    <div className="p-4 bg-white border border-gray-300 rounded-lg shadow">
-      <div className="flex justify-between items-center mb-4">
-        <div>
-          <h2 className="text-xl font-bold text-gray-800">{nanny.name}</h2>
-          <p className="text-sm text-gray-500">{nanny.location}</p>
+    <div className="flex p-6 bg-[#fbfbfb] border rounded-[24px]">
+      <div className="flex-shrink-0 mr-6">
+        <div className="relative w-[96px] h-[96px] rounded-[15px] border-[2px] border-[#f03f3b33]  p-[12px]">
+          <img
+            src={nanny.avatar_url}
+            alt={nanny.name}
+            className="w-full h-full bg-[#e0e0e0] rounded-[15px] object-cover"
+          />
         </div>
-        <p className="text-lg font-medium text-main">${nanny.price_per_hour}</p>
       </div>
-      <div className="text-sm text-gray-600 mb-4">
-        <p>
-          <span className="font-bold">Characters:</span>{" "}
-          {nanny.characters.join(", ")}
-        </p>
-        <p>
-          <span className="font-bold">Education:</span> {nanny.education}
-        </p>
-      </div>
-      <p className="text-sm text-gray-600 mb-4">
-        {nanny.about.length > 100
-          ? `${nanny.about.slice(0, 100)}...`
-          : nanny.about}
-      </p>
-      <div className="text-sm text-gray-600">
-        <p>Age: {nanny.birthday}</p>
-        <p>Experience: {nanny.experience} years</p>
-        <p>Rating: {nanny.rating} ⭐</p>
+
+      <div className="flex-1">
+        <div className="flex direction-row justify-between mb-[8px]">
+          <p className="text-subtitle">Nanny</p>
+          <div className="flex lex direction-row items-center">
+            <Icon id="location" className="w-4 h-4 text-gray-500 mr-2" />
+            <p className="nannies-header-text">{nanny.location}</p>
+            <Icon id="star" className="w-4 h-4 text-yellow-400 mr-1" />
+            <p className="nannies-header-text">Rating: {nanny.rating}</p>
+            <p className="nannies-header-text flex gap-1 pr-[48px]">
+              Price / 1 hour:{" "}
+              <p className="text-[#38cd3e]">{nanny.price_per_hour}$</p>
+            </p>
+            <Icon
+              id="heart"
+              className="w-6 h-6 text-gray-500 cursor-pointer hover:text-red-500"
+            />
+          </div>
+        </div>
+        <div className="pb-[24px]">
+          <h2 className="nannies-name">{nanny.name}</h2>
+        </div>
+        <div className="flex gap-2 flex-wrap pb-[24px]">
+          <div className="nannies-panels">
+            <p className="nannies-panels-title">Age:</p>
+            <p className="nannies-panels-text underline">
+              {new Date().getFullYear() -
+                new Date(nanny.birthday).getFullYear() -
+                (new Date().getMonth() < new Date(nanny.birthday).getMonth() ||
+                (new Date().getMonth() ===
+                  new Date(nanny.birthday).getMonth() &&
+                  new Date().getDate() < new Date(nanny.birthday).getDate())
+                  ? 1
+                  : 0)}
+            </p>
+          </div>
+          <div className="nannies-panels">
+            <p className="nannies-panels-title">Experience:</p>
+            <p className="nannies-panels-text">{nanny.experience}</p>
+          </div>
+          <div className="nannies-panels">
+            <p className="nannies-panels-title">Kids age:</p>
+            <p className="nannies-panels-text">{nanny.kids_age} years old</p>
+          </div>
+          <div className="nannies-panels">
+            <p className="nannies-panels-title">Characters:</p>
+            <p className="nannies-panels-text">{nanny.characters.join(", ")}</p>
+          </div>
+          <div className="nannies-panels ">
+            <p className="nannies-panels-title">Education:</p>
+            <p className="nannies-panels-text">{nanny.education}</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-sm text-gray-600 mb-4">
+            {nanny.about.length > 100
+              ? `${nanny.about.slice(0, 100)}...`
+              : nanny.about}
+          </p>
+        </div>
+        <div>
+          <button className="nannies-read-more">Read more</button>
+        </div>
       </div>
     </div>
   );
