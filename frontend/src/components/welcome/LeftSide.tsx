c1883d7010f7539e1c78b2df0b54d2f04ec5d44e
Dumitru <dima50066@gmail.com>
2025-01-22 12:01:08 +0100

    new icon for hover

--- frontend/src/components/welcome/LeftSide.tsx
+++ frontend/src/components/welcome/LeftSide.tsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
 import Icon from "../../shared/icon/Icon";
+import { useState } from "react";
 
 const LeftSide = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <div className="bg-main rounded-bl-[30px] text-white  rounded-tl-[30px] w-1/2 h-[784px]">
+    <div className="bg-main rounded-bl-[30px] text-white rounded-tl-[30px] w-1/2 h-[784px]">
       <div className="mx-auto w-[517px] mt-[251px] ml-[96px]">
         <h1 className="welcome-title mb-[28px]">
           Make Life Easier for the Family:
@@ -14,9 +25,16 @@ const LeftSide = () => {
         <Link
           to="/nannies"
           className="welcome-btn welcome-btn-text flex items-center gap-[18px] relative"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         >
           Get started
-          <Icon id="checkmark" className="" width={15} height={17} />
+          <Icon
+            id={isHovered ? "checkmark-up" : "checkmark"}
+            className=""
+            width={20}
+            height={20}
+          />
         </Link>
       </div>
     </div>
