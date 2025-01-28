a733b88c8402e6e3a3dfc6a2c908a24b4e898070
Dumitru <dima50066@gmail.com>
2025-01-28 12:56:48 +0100

    add url for requests

--- frontend/src/hooks/axiosConfig.ts
+++ frontend/src/hooks/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://your-production-url.com"
+    ? "https://nanny-pet.onrender.com"
     : "http://localhost:5000";
 
 const axiosInstance = axios.create({
